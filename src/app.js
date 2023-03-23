cb6f7195569a66686c29311fb48d795b82e3a12c
madebypatrick <mugwanezapatrick6@gmail.com>
2023-03-23 12:09:51 +0200

    chore(deploy-base-application): change main from index.js to src/server.js in package.json

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
+app.get('/api/v1', (req, res) => {
   res.status(200).send({
     status: 200,
     success: true,
