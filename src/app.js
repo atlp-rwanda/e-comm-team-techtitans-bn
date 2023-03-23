6a324dea8f69cb3f7420acbb85bb5b5f1ba8e011
neza <tuyishmirend@gmail.com>
2023-03-23 21:43:51 +0200

    solving conflicts

--- src/app.js
+++ src/app.js
@@ -1,6 +1,9 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import swaggerUi from "swagger-ui-express";
+import combinedDocs from "../docs/index.js"
+
 
 dotenv.config();
 
@@ -18,5 +21,7 @@ app.get('/', (req, res) => {
     message: `Welcome to team Tech-Titans's API! Endpoints available at http://localhost:${PORT}/api/v1 + the endpoint you want to hit`,
   });
 });
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(combinedDocs));
 
 export default app;
