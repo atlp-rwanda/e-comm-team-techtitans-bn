8f9c37873874047727758e6501d75634b1925e31
EdwinBayingana <bayinganaedwin@gmail.com>
2023-03-25 12:41:30 +0200

    fix(boilerplate-restructure): restructure our package.js and app.js files

--- src/app.js
+++ src/app.js
@@ -1,13 +1,12 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import swaggerUi from "swagger-ui-express";
-import combinedDocs from "../docs/index.js"
-
+import swaggerUi from 'swagger-ui-express';
+import combinedDocs from '../docs/index.js';
 
 dotenv.config();
 
-const { PORT } = process.env;
+// const { PORT } = process.env;
 const app = express();
 app.use(cors());
 
@@ -15,13 +14,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
-  res.status(200).send({
-    status: 200,
-    success: true,
-    message: `Welcome to team Tech-Titans's API! Endpoints available at http://localhost:${PORT}/api/v1 + the endpoint you want to hit`,
-  });
+  res
+    .status(200)
+    .send(
+      `<h1 style="text-align: center; color: #CCD6F6; margin-top: 20vh; background: #0A192F; padding: 150px;">Welcome to team Tech-Titans's E-commerce API!</h1>`,
+    );
 });
 
+// app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(combinedDocs));
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(combinedDocs));
 
 export default app;
--- src/server.js
+++ src/server.js
@@ -1,37 +1,7 @@
-import app from './app.js';
+import app from './app';
 
 const { PORT } = process.env;
 
-const start = () => {
-    try {
-        app.listen(PORT, () => {
-      console.log(`🍏 Server is running on: http://localhost:${PORT} ... 🌊`);
-        });
-        // Catch all routes that were not found and send this response
-        app.use((req, res) => {
-            res.status(404).json({
-                status: 404,
-                success: false,
-                message: "🍎 Route doesn't exist.😬",
-            });
-        });
-    } catch (error) {
-        if (error) {
-            console.log(`🍎 Error occurred when starting server: ${error.message}`);
-        } else {
-            return error.message;
-        }
-    }
-};
-
-start();
-
-// "scripts": {
-//     "start": "nodemon src/server.js",
-//     "lint:fix": "eslint src --fix --cache",
-//     "test": "jest",
-//     "dev": "nodemon --exec babel-node src/server.js"
-//   },
-
-// "build": "babel src --out-dir lib",
-// "start": "npm run build && node lib/server.js"
+app.listen(PORT, () => {
+  console.log(`🍏 Server is running on: http://localhost:${PORT} ... 🌊`);
+});
