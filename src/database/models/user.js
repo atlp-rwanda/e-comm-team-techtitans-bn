501f07f0669a3a59eca94a9547e1c4416e41b40b
madebypatrick <mugwanezapatrick6@gmail.com>
2023-03-27 17:26:06 +0200

    add profile feature

--- src/database/models/user.js
+++ src/database/models/user.js
@@ -1,30 +1,46 @@
-'use strict';
-const { Model } = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
-    class User extends Model {
-        /**
-         * Helper method for defining associations.
-         * This method is not a part of Sequelize lifecycle.
-         * The `models/index` file will call this method automatically.
-         */
-        static associate(models) {
-            // define association here
-        }
+  class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate({ Profile }) {
+      this.hasOne(Profile, {
+        foreignKey: "userId",
+      });
     }
-    User.init(
-        {
-            fullname: DataTypes.STRING,
-            email: DataTypes.STRING,
-            password: DataTypes.STRING,
-            role: {
-                type: DataTypes.INTEGER,
-                defaultValue: 1,
-            },
-        },
-        {
-            sequelize,
-            modelName: 'User',
-        },
-    );
-    return User;
+
+    toJSON() {
+      return { ...this.get(), id: undefined };
+    }
+  }
+  User.init(
+    {
+      uuid: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+      },
+      fullname: DataTypes.STRING,
+      email: DataTypes.STRING,
+      password: DataTypes.STRING,
+      gender: DataTypes.STRING,
+      birthdate: DataTypes.DATE,
+      preferredLanguage: DataTypes.STRING,
+      preferredCurrency: DataTypes.STRING,
+      location: DataTypes.STRING,
+      billingAddress: DataTypes.STRING,
+      role: {
+        type: DataTypes.INTEGER,
+        defaultValue: 1,
+      },
+    },
+    {
+      sequelize,
+      modelName: "User",
+    }
+  );
+  return User;
 };
--- tests/user/signup_login.test.js
+++ tests/user/signup_login.test.js
@@ -1,58 +1,76 @@
 import request from 'supertest';
 import app from '../../src/app';
-import {successRegistration, theSuccessLoginCredentials, unSuccessfullLoginCredentials} from '../mocks/user.mock';
+import { successRegistration, theSuccessLoginCredentials, unSuccessfullLoginCredentials, profile } from '../mocks/user.mock';
 import { expect, describe, test } from '@jest/globals';
-let userTokens='';
+
+let userTokens = '';
+let userUuid = '';
+const uuidRegex = new RegExp(
+  '^[a-f0-9]{8}-[a-f0-9]{4}-[1-5][a-f0-9]{3}-[89ab][a-f0-9]{3}-[a-f0-9]{12}$',
+  'i'
+);
+
 describe('User Test (Signup and login)', () => {
-    /*
+  /*
    **********************************************
    *  🟢 Signup a new User *
    **********************************************
    */
-    test('Successful Signup', async () => {
-        const response = await request(app)
-            .post('/api/v1/user/signup')
-            .send(successRegistration);
-        // console.log(response)
-        const token=response.body.data.userToken;
-        userTokens=token;
-        expect(response.statusCode).toBe(200);
-    });
+  test('Successful Signup', async () => {
+    const response = await request(app)
+      .post('/api/v1/user/signup')
+      .send(successRegistration);
+    const token = response.body.data.userToken;
 
-    test('Successful Verification', async () => {
-        const response = await request(app)
-            .get(`/api/v1/user/signup/${userTokens}`)
-        expect(response.statusCode).toBe(201);
-    });
-    /*
+    userTokens = token;
+    expect(response.statusCode).toBe(200);
+  });
+
+  test('Successful Verification', async () => {
+    const response = await request(app).get(`/api/v1/user/signup/${userTokens}`);
+    expect(response.statusCode).toBe(201);
+    expect(response.body.data).toHaveProperty('uuid');
+    expect(response.body.data.uuid).toMatch(uuidRegex);
+
+    userUuid = response.body.data.uuid;
+    expect(userUuid).toBeDefined();
+  });
+  /*
    **********************************************
    * 🛑 End Signup a new User *
    **********************************************
    */
 
-    /*
+  /*
   **********************************************
   *  🟢 Login for verified User *
   **********************************************
   */
-    test('Successful Login', async () => {
-        const response = await request(app)
-            .post('/api/v1/user/login')
-            .send(theSuccessLoginCredentials);
-        expect(response.statusCode).toBe(200);
-    });
+  test('Successful Login', async () => {
+    const response = await request(app)
+      .post('/api/v1/user/login')
+      .send(theSuccessLoginCredentials);
+    expect(response.statusCode).toBe(200);
+  });
 
-    // Login for unverified User
+  // Login for unverified User
 
-    test('Unsuccessful Login', async () => {
-        const response = await request(app)
-            .post('/api/v1/user/login')
-            .send(unSuccessfullLoginCredentials);
-        expect(response.statusCode).toBe(401);
-    });
-    /*
+  test('Unsuccessful Login', async () => {
+    const response = await request(app)
+      .post('/api/v1/user/login')
+      .send(unSuccessfullLoginCredentials);
+    expect(response.statusCode).toBe(401);
+  });
+  /*
   **********************************************
   * 🛑 End login test *
   **********************************************
   */
+
+  test('Successful update', async () => {
+    const response = await request(app)
+      .put(`/api/v1/user/${userUuid}`)
+      .send(profile);
+    expect(response.statusCode).toBe(201);
+  });
 });
