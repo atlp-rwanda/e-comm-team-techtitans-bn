ddc46a422a0bdf71e7cda95cf86940c6c1db329a
madebypatrick <mugwanezapatrick6@gmail.com>
2023-03-27 17:26:06 +0200

    add profile feature

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,6 @@
 import express from 'express';
 
-import { verifyUser, createUser,login } from '../controllers/user.controller';
+import { verifyUser, createUser,updateProfile,findAllUsers, login } from '../controllers/user.controller';
 
 const userRouter = express.Router();
 
@@ -8,5 +8,6 @@ const userRouter = express.Router();
 userRouter.post('/signup', verifyUser);
 userRouter.get('/signup/:token', createUser);
 userRouter.post('/login',login)
-
+userRouter.put('/:uuid', updateProfile);
+userRouter.get('/profile/users',findAllUsers);
 export default userRouter;
